a278be79633044f0a5f29a7df3878cc0e69d3af3
Nikolai <niklonberg@gmail.com>
2024-04-19 14:57:16 +0200

    add strictmode, GradientGenerator component

---
+++ src/components/GradientGenerator/GradientGenerator.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function GradientGenerator() {
+  return <div></div>;
+}
+
+export default GradientGenerator;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 
 import App from "./components/App";
 import "./reset.scss";
 import "./styles.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
